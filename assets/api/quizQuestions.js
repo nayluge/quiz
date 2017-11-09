3e30802a516c3d3b6c177b54f580216e9cd46a39
nayluge <cyril.souillard@gmail.com>
2017-11-10 00:49:04 +0100

    Initial commit

---
+++ assets/api/quizQuestions.js
@@ -0,0 +1,94 @@
+var quizQuestions = [
+  {
+      question: "What franchise would you rather play a game from?",
+      answers: [
+          {
+              type: "Microsoft",
+              content: "Halo"
+          },
+          {
+              type: "Nintendo",
+              content: "Pokemon"
+          },
+          {
+              type: "Sony",
+              content: "Uncharted"
+          }
+      ],
+      renderer: "text"
+  },
+  {
+      question: "Which console would you prefer to play with friends?",
+      answers: [
+          {
+              type: "Microsoft",
+              content: "X-Box"
+          },
+          {
+              type: "Nintendo",
+              content: "Nintendo 64"
+          },
+          {
+              type: "Sony",
+              content: "Playstation 1"
+          }
+      ],
+      renderer: "text"
+  },
+  {
+      question: "Which of these racing franchises would you prefer to play a game from?",
+      answers: [
+          {
+              type: "Microsoft",
+              content: "Forza"
+          },
+          {
+              type: "Nintendo",
+              content: "Mario Kart"
+          },
+          {
+              type: "Sony",
+              content: "Gran Turismo"
+          }
+      ],
+      renderer: "text"
+  },
+  {
+      question: "Which of these games do you think is best?",
+      answers: [
+          {
+              type: "Microsoft",
+              content: "BioShock"
+          },
+          {
+              type: "Nintendo",
+              content: "The Legend of Zelda: Ocarina of Time"
+          },
+          {
+              type: "Sony",
+              content: "Final Fantasy VII"
+          }
+      ],
+      renderer: "text"
+  },
+  {
+      question: "What console would you prefer to own?",
+      answers: [
+          {
+              type: "Microsoft",
+              content: "X-Box One"
+          },
+          {
+              type: "Nintendo",
+              content: "Wii U"
+          },
+          {
+              type: "Sony",
+              content: "Playstation 4"
+          }
+      ],
+      renderer: "text"
+  }
+];
+
+export default quizQuestions;
---
+++ public/test.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Title</title>
+    <link rel="stylesheet" href="build/index.css">
+    <link rel="stylesheet" href="build/app.css">
+</head>
+<body>
+<div id="root"></div>
+<script type="text/javascript" src="build/app.js"></script>
+</body>
+</html>
